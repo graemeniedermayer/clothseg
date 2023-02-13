59dbe53647bef91df285179516ae8838339c6000
ClashSAN <98228077+ClashSAN@users.noreply.github.com>
2023-02-13 04:45:05 +0000

    Update and rename launch.py to install.py
    dependency clashes with Pillow, numpy, and some others.

---
+++ install.py
@@ -0,0 +1,5 @@
+
+import launch
+
+if not launch.is_installed("rembg"):
+    launch.run_pip("install rembg", "installed requirements for clothseg")
--- launch.py
+++
@@ -1,2 +0,0 @@
-import launch
-launch.git_clone("https://github.com/danielgatis/rembg", "repositories/rembg", "rembg")
